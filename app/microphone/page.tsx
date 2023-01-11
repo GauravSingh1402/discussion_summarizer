fba4c597740f23b4153dab277dd8cbd372e0fe29
Advait Nurani <advaitnurani@gmail.com>
2023-01-11 12:51:23 +0530

    Speechly integration

---
+++ app/microphone/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import "regenerator-runtime/runtime";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const appId = "41c98519-9d2c-49a8-891c-606cfb75c911";
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
+
+const Page = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+export default Page;
