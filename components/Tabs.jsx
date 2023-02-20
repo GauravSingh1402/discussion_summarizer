5fb113a792753287ebe1d4cca525955c7acbccdf
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-02-20 12:01:21 +0530

    Tabs done

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,105 +1,124 @@
-import React from 'react'
+import { useState } from 'react'
+import { Tab } from '@headlessui/react'
 
-const Tabs = () => {
-
-
-let tabs = document.querySelectorAll(".tab")
-let indicator = document.querySelector(".indicator")
-let panels = document.querySelectorAll(".tab-panel")
-
-indicator.style.width = tabs[0].getBoundingClientRect().width + 'px'
-indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + 'px'
-
-tabs.forEach(tab =>{
-  tab.addEventListener("click", ()=>{
-    let tabTarget = tab.getAttribute("aria-controls")
-
-    indicator.style.width = tab.getBoundingClientRect().width + 'px'
-    indicator.style.left = tab.getBoundingClientRect().left - tab.parentElement.getBoundingClientRect().left + 'px'
-
-
-    panels.forEach(panel =>{
-      let panelId = panel.getAttribute("id")
-      if(tabTarget === panelId){
-        panel.classList.remove("invisible", "opacity-0")
-        panel.classList.add("visible", "opacity-100")
-      } else {
-        panel.classList.add("invisible", "opacity-0")
-      }
-    })
-  })
-})
-  return (
-    <div class="min-h-screen flex items-center bg-white">
-    <div class="max-w-3xl mx-auto px-8 sm:px-0">
-      <div class="sm:w-11/12 sm:mx-auto">
-        <div 
-          role="tablist"
-          aria-label="tabs"
-          class="relative w-max mx-auto h-12 grid grid-cols-3 items-center px-[3px] rounded-md bg-[#E5E5E5] overflow-hidden shadow-2xl shadow-900/20 transition"
-        >
-          <div class="absolute indicator h-11 my-auto top-0 bottom-0 left-0 rounded-md bg-gradient-to-r from-[#ff0f7b] to-[#f89b29] text-white shadow-md"></div>
-          <button
-            role="tab"
-            aria-selected="true"
-            aria-controls="panel-1"
-            id="tab-1"
-            tabindex="0"
-            class="relative block h-10 px-6 tab rounded-full"
-          >
-            <span class="text-gray-800">Text</span>
-          </button>
-          <button
-            role="tab"
-            aria-selected="false"
-            aria-controls="panel-2"
-            id="tab-2"
-            tabindex="-1"
-            class="relative block h-10 px-6 tab rounded-full"
-          >
-            <span class="text-gray-800">Audio</span>
-          </button>
-          <button
-            role="tab"
-            aria-selected="false"
-            aria-controls="panel-3"
-            id="tab-3"
-            tabindex="-1"
-            class="relative block h-10 px-6 tab rounded-full"
-          >
-            <span class="text-gray-800">Video</span>
-          </button>
-        </div>
-        <div class="mt-6 relative rounded-3xl bg-purple-50">
-          <div
-            role="tabpanel"
-            id="panel-1"
-            class="tab-panel p-6 transition duration-300"
-          >
-            <h2 class="text-xl font-semibold text-gray-800">First tab panel</h2>
-            <p class="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas dolores voluptate temporibus, atque ab eos, delectus at ad hic voluptatem veritatis iure, nulla voluptates quod nobis doloremque eaque! Perferendis, soluta.</p>
-          </div>
-          <div
-            role="tabpanel"
-            id="panel-2"
-            class="absolute top-0 invisible opacity-0 tab-panel p-6 transition duration-300"
-          >
-            <h2 class="text-xl font-semibold text-gray-800">Second tab panel</h2>
-            <p class="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas dolores voluptate temporibus, atque ab eos, delectus at ad hic voluptatem veritatis iure, nulla voluptates quod nobis doloremque eaque! Perferendis, soluta.</p>
-          </div>
-          <div
-            role="tabpanel"
-            id="panel-3"
-            class="absolute top-0 invisible opacity-0 tab-panel p-6 transition duration-300"
-          >
-            <h2 class="text-xl font-semibold text-gray-800">Third tab panel</h2>
-            <p class="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas dolores voluptate temporibus, atque ab eos, delectus at ad hic voluptatem veritatis iure, nulla voluptates quod nobis doloremque eaque! Perferendis, soluta.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  )
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Tabs
+export default function Example() {
+  let [categories] = useState({
+    Recent: [
+      {
+        id: 1,
+        title: 'Does drinking coffee make you smarter?',
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: '2h ago',
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    Popular: [
+      {
+        id: 1,
+        title: 'Is tech making coffee better or worse?',
+        date: 'Jan 7',
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: 'The most innovative things happening in coffee',
+        date: 'Mar 19',
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+    Trending: [
+      {
+        id: 1,
+        title: 'Ask Me Anything: 10 answers to your questions about coffee',
+        date: '2d ago',
+        commentCount: 9,
+        shareCount: 5,
+      },
+      {
+        id: 2,
+        title: "The worst advice we've ever heard about coffee",
+        date: '4d ago',
+        commentCount: 1,
+        shareCount: 2,
+      },
+    ],
+  })
+
+  return (
+    <div className="w-full max-w-md px-2 py-16 mx-auto sm:px-0">
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+          {Object.keys(categories).map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                  selected
+                    ? 'bg-white shadow'
+                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                'rounded-xl bg-white p-3',
+                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+              )}
+            >
+              <ul>
+                {posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="relative rounded-md p-3 hover:bg-gray-100"
+                  >
+                    <h3 className="text-sm font-medium leading-5">
+                      {post.title}
+                    </h3>
+
+                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                      <li>{post.date}</li>
+                      <li>&middot;</li>
+                      <li>{post.commentCount} comments</li>
+                      <li>&middot;</li>
+                      <li>{post.shareCount} shares</li>
+                    </ul>
+
+                    <a
+                      href="#"
+                      className={classNames(
+                        'absolute inset-0 rounded-md',
+                        'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
+                      )}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import Tabs from '../components/Tabs'
 export default function Home() {
