4ea073a15b049b5da12da20b5054ecb4c04fe48b
Gaurav Singh <gaurav.s.140102@gmail.com>
2023-02-21 20:08:45 +0530

    Move assets folder to public and fix dependency issues

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,47 +1,27 @@
-import React from "react";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { AiOutlineClose } from "react-icons/ai";
-
-import logo from "../assets/logo.svg";
+import React, { useState } from "react";
+import logoBlack from "../public/assets/logo-black.svg";
+import Image from "next/image";
 
 const NavBarItem = ({ title, classprops }) => (
-    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+	<li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
 );
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = React.useState(false);
+	// const [toggleMenu, setToggleMenu] = React.useState(false);
+	
 
-    return (
-        <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="md:flex-[0.5] flex-initial justify-center items-center">
-                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
-            </div>
-            <ul className="text-black md:flex hidden list-none flex-row justify-between items-center flex-initial">
-                {["Login"].map((item, index) => (
-                    <NavBarItem key={item + index} title={item} />
-                ))}
-                <li className="bg-gradient-to-r from-[#ff0f7b] to-[#f89b29]  py-2 px-7 mx-4 rounded-lg cursor-pointer text-[#fafafa] hover:bg-[#2546bd]">
-                    Generate Summary
-                </li>
-            </ul>
-            <div className="flex relative">
-                {!toggleMenu && (
-                    <HiMenuAlt4 fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
-                )}
-                {toggleMenu && (
-                    <ul
-                        className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md text-black animate-slide-in"
-                    >
-                        <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-                        {["Login","Generate Summary"].map(
-                            (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-                        )}
-                    </ul>
-                )}
-            </div>
-        </nav>
-    );
+	return (
+		<nav className="w-full flex justify-between p-6">
+			<div className="w-1/6">
+				<Image src={logoBlack} />
+			</div>
+			<div>
+				<button className="font-sans bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end text-white px-4 py-2 rounded-md font-semibold hover:scale-105 transition-all shadow-lg">
+					Generate Summary
+				</button>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				light: {
+					primary: "#fafafa",
+					secondary: "#E5E5E5",
+					content: "#121212",
+				},
+				dark: {
+					primary: "#121212",
+					secondary: "#373737",
+					content: "#fafafa",
+				},
+				"custom-gradient": {
+					start: "#ff0f7b",
+					end: "#f89b29",
+				},
+				fontFamily: {
+					sans: ["Inter", "sans-serif"],
+					heading: ["Poppins", "sans-serif"],
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {
+			textColor: ["dark"],
+			backgroundColor: ["dark"],
+		},
+	},
+	plugins: [],
+};
