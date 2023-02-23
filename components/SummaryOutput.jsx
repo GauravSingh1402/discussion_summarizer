a33fd538ca7242956e8e39c8f942771faf96780a
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-02-23 13:49:00 +0530

    title added in Summary output

--- components/SummaryOutput.jsx
+++ components/SummaryOutput.jsx
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes";
 const SummaryOutput = () => {
 	const router = useRouter();
 	const { theme, setTheme } = useTheme();
-	const { kl, lsa } = router.query;
+	const { kl, lsa, title } = router.query;
 	return (
 		<div className="flex flex-col items-center w-full px-5 py-8 justify-center gap-10">
 			<h1 className="text-3xl text-transparent bg-clip-text font-heading font-bold bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end">
@@ -17,6 +17,26 @@ const SummaryOutput = () => {
 				<div
 					className={`relative w-full rounded-lg mt-5 bg-${theme}-secondary flex flex-col p-8`}
 				>
+				<h2 className="font-heading font-medium text-left my-3">
+					Generated Title
+				</h2>
+				<div className='flex justify-between '>
+					<div className='text-justify w-[70%] md:w-[75%] bg-white p-7 text-black rounded-md font-small min-h-[100px] max-h-[100px] h-[100px] overflow-y-auto'>
+					{title}
+				</div>
+				<div className='flex flex-col'>
+					<button
+					className=" mb-5 bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end text-white px-4 py-2 rounded-md font-semibold hover:scale-105 transition-all shadow-lg"
+				>
+					Copy To ClipBoard
+				</button>
+					<button
+					className="bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end text-white px-4 py-2 rounded-md font-semibold hover:scale-105 transition-all shadow-lg"
+				>
+					Save To Profile
+				</button>
+				</div>
+				</div>
 				<h2 className="font-heading font-medium text-left my-3">
 					Top Summary
 				</h2>
