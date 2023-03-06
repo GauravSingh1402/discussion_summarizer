02ba0e81c8f83899b37d5a4bcb49d574e2b006ed
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-03-06 11:10:04 +0530

    Setup route for accounts page

---
+++ pages/account.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Account from '../components/Account';
+import Layout from '../components/Layout';
+
+const account = () => {
+    return (
+        <Layout>
+            <Account />
+        </Layout>
+    );
+}
+
+export default account;
