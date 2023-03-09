8649a52bf61022f76ac78d77d378dca6e2e85d5e
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-03-09 19:49:09 +0530

    Video uploader and form done

--- components/AudioFormComp/Uploader.jsx
+++ components/AudioFormComp/Uploader.jsx
@@ -121,15 +121,15 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
     } catch (error) {}
   };
   return (
-    <div className="w-[90%] p-3">
+    <div className="w-[90%] py-2 sm:p-3">
       {switchDisplay == "upload" && (
         <label
-          className={`flex justify-center w-full h-[300px] px-4 transition bg-${theme}-primary border-2 border-gray-500 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-300 focus:outline-none`}
+          className={`flex justify-center w-full h-[250px] sm:h-[300px] px-4 transition bg-${theme}-primary border-2 border-gray-500 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-300 focus:outline-none`}
         >
           <span className="flex flex-col items-center justify-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-16 h-16 text-blue-600"
+              className="w-10 sm:w-16 h-10 sm:h-16 text-content"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -141,12 +141,12 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
                 d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
               />
             </svg>
-            <span className={`font-medium text-content text-center`}>
+            <span className={`text-sm sm:text-md font-medium text-content text-center`}>
               Drop files to Attach, <br /> or
               <span className="text-blue-600 underline ml-2">browse</span>
               <br />
             </span>
-            <span className={`font-medium text-gray-500`}>
+            <span className={`text-xs sm:text-sm font-medium text-gray-500 text-center`}>
               Supports .mp3, .ogg, .wav, .flac
             </span>
           </span>
@@ -164,19 +164,19 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
         <div className="flex flex-col">
           {switchDisplay == "preview" ? (
             <div
-              className={`bg-gray-500 w-full flex flex-col justify-center p-5 rounded-lg`}
+              className={`w-full flex flex-col justify-center px-1 py-5 sm:p-5 rounded-lg`}
             >
                {audioSrc && (
-        <audio className="w-full" controls>
+        <audio className={theme=='light' ? "w-full  bg-black p-1 rounded-full" : "w-full bg-white p-1 rounded-full"} controls>
           <source src={audioSrc} type={getMimeType(audioSrc)} />
           Your browser does not support the audio tag.
         </audio>
       )}
-              <div className='flex flex-row items-center justify-center px-5'>
+              <div className='mt-1 flex flex-row items-center justify-center px-3 sm:px-5'>
                 <div className='w-full flex flex-col justify-center'>
                   <div className="flex flex-row items-center justify-between">
-                <p className="text-white">{fileName}</p>
-                <p className="italic text-gray-300">{progressText}</p>
+                <p className="text-content truncate w-[60%] text-sm sm:text-md">{fileName}</p>
+                <p className="italic text-content text-xs sm:text-sm text-right">{progressText}</p>
               </div>
               {uploadProgress > 0 && (
                 <div className="my-2 w-full h-2 bg-gray-300 rounded-full">
@@ -187,13 +187,13 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
                 </div>
               )}
               <div>
-                <p className="text-gray-300 text-xs"> {fileInMb}</p>
+                <p className="text-content text-xs"> {fileInMb}</p>
               </div>
                 </div>
               </div>
             </div>
           ) : (
-            <div className="w-full flex flex-col items-center justify-center">
+            <div className="my-2 w-full flex flex-col items-center justify-center">
               <svg
                 className="animate-spin text-content w-20 h-20 feather feather-loader"
                 fill="none"
@@ -226,7 +226,7 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
           ) : (
             <div className="flex w-full justify-between my-5">
               <button
-                className="font-heading border-x-custom-gradient-start border-2 border-y-custom-gradient-end w-[45%] text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end bg-white rounded-md px-5 py-2 hover:scale-110 transition-all"
+                className="font-heading border-x-custom-gradient-start border-2 border-y-custom-gradient-end w-[45%] text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end bg-white rounded-md px-2 py-1 sm:px-5 sm:py-2 text-sm sm:text-lg hover:scale-110 transition-all"
                 onClick={() => {
                   setswitchDisplay("upload");
                 }}
@@ -234,7 +234,7 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
                 Back
               </button>
               <button
-                className="font-heading text-white w-[45%] bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end rounded-md px-5 py-2 hover:scale-110 transition-all"
+                className="font-heading text-white w-[45%] bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end rounded-md px-2 py-1 sm:px-5 sm:py-2 text-sm sm:text-lg hover:scale-110 transition-all"
                 onClick={() => {
                   handleFileUpload();
                 }}
@@ -251,7 +251,7 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
         <div className="w-full flex flex-col items-center justify-center">
           <textarea
             rows={8}
-            className="bg-light-primary rounded-md p-5 w-[80%] flex-wrap text-black"
+            className="text-xs sm:text-md md:text-lg bg-light-primary rounded-md p-5 w-[90%] sm:w-[80%] flex-wrap text-black mt-4"
             placeholder="Add your content here"
             name="text"
             type="text"
@@ -261,9 +261,9 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
               setValues({ ...values, text: e.target.value });
             }}
           />
-          <div className="flex w-[20%] justify-between my-5">
+          <div className="my-5 flex w-full flex gap-2 justify-center items-center">
             <button
-              className="font-heading border-x-custom-gradient-start border-2 border-y-custom-gradient-end w-[45%] text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end bg-white rounded-md px-5 py-2 hover:scale-110 transition-all"
+              className="font-heading border-x-custom-gradient-start border-2 border-y-custom-gradient-end w-[45%] text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end bg-white rounded-md px-2 py-1 sm:px-5 sm:py-2 text-sm sm:text-lg hover:scale-110 transition-all"
               onClick={() => {
                 setswitchDisplay("upload");
               }}
@@ -271,7 +271,7 @@ const Uploader = ({ onSubmit, onPrev, data }) => {
               Back
             </button>
             <button
-              className="font-heading text-white w-[45%] bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end rounded-md px-5 py-2 hover:scale-110 transition-all"
+              className="font-heading text-white w-[45%] bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end rounded-md px-2 py-1 sm:px-5 sm:py-2 text-sm sm:text-lg hover:scale-110 transition-all"
               onClick={handleSubmit}
             >
               Next
