fb6bcdf9cbed6a7ac4a31e1afedec662d81bee28
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-03-09 12:07:12 +0530

    Landing Page responsiveness fixed

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -10,7 +10,33 @@ const LandingPage = () => {
 	const { theme, setTheme } = useTheme();
 	return (
 		<div className="w-full flex flex-col items-center justify-center">
-			<div className="w-full flex flex-col sm:flex-row  justify-evenly pt-20 pb-24">
+			<div className='sm:hidden w-full flex flex-col items-center justify-center gap-3 my-10 px-10'>
+				<h3 className="text-2xl text-center font-extrabold font-heading text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end">
+					Effortlessly Summarize Any Media With AI
+				</h3>
+				<div className="relative w-[20%] h-[25%]">
+					<div
+						className={`absolute inset-0 w-full rounded-lg bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end blur-xl opacity-50 flex justify-center align-center`}
+					></div>
+					<div className={`relative w-full rounded-lg flex flex-col`}>
+						<Image className="cursor-pointer" src={videofolder} />
+					</div>
+				</div>
+				<h3 className="md:text-lg text-center font-sans">
+					Get the gist of any text, audio or video in seconds with Summasense,
+					the AI-powered summarization tool
+				</h3>
+				<button
+					onClick={() => {
+						Router.push("/generateSummary");
+					}}
+					className="w-fit flex items-center gap-2 bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end text-white px-8 py-4 rounded-md font-semibold hover:scale-105 transition-all shadow-lg"
+				>
+					Generate Summary For Free
+					<ArrowRightCircleIcon className="w-6 h-6 stroke-2" />
+				</button>
+			</div>
+			<div className="hidden w-full sm:flex flex-col sm:flex-row  justify-evenly pt-20 pb-24">
 				<div className="w-full flex flex-col justify-between sm:w-[40%]">
 					<h3 className="text-2xl sm:text-5xl font-extrabold font-heading text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end">
 						Effortlessly Summarize Any Media With AI
@@ -47,13 +73,13 @@ const LandingPage = () => {
 						In Action
 					</span>
 				</h3>
-				<div className="flex items-center justify-evenly">
+				<div className="flex flex-col sm:flex-row items-center justify-evenly">
 					<div className="relative w-fit">
 						<div
 							className={`absolute inset-0 w-full rounded-lg bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end blur-xl opacity-50 flex justify-center align-center`}
 						></div>
 						<div
-							className={`relative rounded-lg flex flex-col w-[400px] h-[350px] bg-white`}
+							className={`relative rounded-lg flex flex-col w-[200px] h-[150px] sm:w-[300px] sm:h-[250px] md:w-[400px] md:h-[350px] bg-white`}
 						>
 							<Image
 								width={400}
@@ -67,7 +93,7 @@ const LandingPage = () => {
 						<h3 className="text-center text-xl sm:text-2xl md:text-3xl font-medium font-heading my-5">
 							Choose The Media
 						</h3>
-						<h3 className="text-sm md:text-lg font-sans text-justify">
+						<h3 className="text-sm md:text-lg font-sans text-justify mx-5">
 							Get the gist of any text, audio or video in seconds with
 							Summasense, the AI-powered summarization tool Get the gist of any
 							text, audio or video in seconds with Summasense, the AI-powered
@@ -79,12 +105,12 @@ const LandingPage = () => {
 					</div>
 				</div>
 			</div>
-			<div className={`w-full flex flex-row align-center justify-evenly py-10`}>
+			<div className={`w-full flex flex-col sm:flex-row items-center justify-evenly py-10`}>
 				<div className="w-full flex flex-col justify-evenly gap-10 sm:w-[50%] align-center">
 					<h3 className="text-center text-xl sm:text-2xl md:text-3xl font-medium font-heading my-5">
 						Lorem Ipsum
 					</h3>
-					<h3 className="text-sm md:text-lg font-sans text-justify">
+					<h3 className="text-sm md:text-lg font-sans text-justify mx-5">
 						Get the gist of any text, audio or video in seconds with Summasense,
 						the AI-powered summarization tool Get the gist of any text, audio or
 						video in seconds with Summasense, the AI-powered summarization tool
@@ -98,7 +124,7 @@ const LandingPage = () => {
 						className={`absolute inset-0 w-full rounded-lg bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end blur-xl opacity-50 flex justify-center align-center`}
 					></div>
 					<div
-						className={`relative rounded-lg flex flex-col w-[400px] h-[350px] bg-white`}
+						className={`relative rounded-lg flex flex-col w-[200px] h-[150px] sm:w-[300px] sm:h-[250px] md:w-[400px] md:h-[350px] bg-white`}
 					>
 						<Image
 							width={300}
@@ -110,7 +136,7 @@ const LandingPage = () => {
 				</div>
 			</div>
 			<div
-				className={`w-full flex flex-col sm:flex-row  justify-evenly py-10 bg-${theme}-secondary`}
+				className={`w-full flex flex-col sm:flex-row items-center justify-evenly py-10 bg-${theme}-secondary`}
 			>
 				<div className="relative w-[70%] sm:w-[30%] h-[20%]">
 					<div
@@ -126,10 +152,10 @@ const LandingPage = () => {
 					</div>
 				</div>
 				<div className="w-full flex flex-col py-5 justify-between sm:w-[50%]">
-					<h3 className="text-2xl sm:text-3xl font-semibold font-heading text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end">
+					<h3 className="text-center sm:text-left text-2xl sm:text-3xl font-semibold font-heading text-transparent bg-clip-text bg-gradient-to-r from-custom-gradient-start to-custom-gradient-end">
 						Unleash The Power Of AI Summarization With SummaSense
 					</h3>
-					<h3 className="md:text-lg font-sans">
+					<h3 className="text-center sm:text-left md:text-lg font-sans">
 						Get the gist of any text, audio or video in seconds with Summasense,
 						the AI-powered summarization tool
 					</h3>
